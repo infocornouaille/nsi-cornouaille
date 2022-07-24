a33dce815a4ffa17cb6291f860f05cde7fc36765
David Couronné <18193744+DavidCouronne@users.noreply.github.com>
2022-07-24 12:07:06 +0200

    Update gen2.py

--- includes/files/gen2.py
+++ includes/files/gen2.py
@@ -3,34 +3,35 @@ import sys
 
 NB_DOSS = 40
 
-lst = ['{:02d}'.format(k) for k in range(1, NB_DOSS+1)]
+lst = ["{:02d}".format(k) for k in range(1, NB_DOSS + 1)]
+
 
 def create_doss():
     for nom in lst:
-        os.makedirs(nom + '_1', exist_ok = True)
-        os.makedirs(nom + '_2', exist_ok = True)
+        os.makedirs(nom + "_1", exist_ok=True)
+        os.makedirs(nom + "_2", exist_ok=True)
+
 
 def fichier(nom_doss):
-    nom_file = nom_doss + '/' + 'enonce.md'
-    with open(nom_file, 'w') as f:
+    nom_file = nom_doss + "/" + "enonce.md"
+    with open(nom_file, "w") as f:
         f.write("")
-        
-    nom_file = nom_doss + '/' + 'correction.md'
-    with open(nom_file, 'w') as f:
+
+    nom_file = nom_doss + "/" + "correction.md"
+    with open(nom_file, "w") as f:
         f.write("")
+
 
 def fichiers_1_2():
     for nom_doss in lst:
-        n1 = nom_doss + '_1'
-        n2 = nom_doss + '_2'
+        n1 = nom_doss + "_1"
+        n2 = nom_doss + "_2"
         fichier(n1)
         fichier(n2)
 
 
 create_doss()
 fichiers_1_2()
-
-
 
 
 contenu = """
@@ -61,11 +62,9 @@ contenu = """
         """
 
 
-with open('BNS_2022.md', 'w') as f:
+with open("BNS_2022.md", "w") as f:
     for val in lst:
         f.write(contenu.format(val))
-
-
 
 
 # nom_dossier = 'listes_logins'
@@ -74,10 +73,10 @@ with open('BNS_2022.md', 'w') as f:
 # os.makedirs(nom_sources, exist_ok = True)
 # for file in os.scandir(nom_dossier):
 #     os.remove(file.path)
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # for classe in classes:
 #     print(classe)
 #     nomhtml = nom_sources + '/' + classe + '.html'
